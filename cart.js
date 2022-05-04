85a496093b2a76ed010751b39a751c2ea53bf801
Atacan Bilgen <atacanbilgen@gmail.com>
2022-05-04 10:48:43 +0200

    Changed to products menu to images with the food descriptions

--- cart.js
+++ cart.js
@@ -65,7 +65,7 @@ function addItemToCart(title, price, imageSrc) {
     var cartItems = document.getElementsByClassName('cart-items')[0];
     var cartItemsNames = cartItems.getElementsByClassName('cart-item-title');
     for (var i = 0; i < cartItemsNames.length; i++) {
-        if (cartItemsNames[i].innerText == title) { //TODO: If the item aldready exists, increase the count of item.
+        if (cartItemsNames[i].innerText == title) { 
             quantityInputs += 1;
         }
     }
